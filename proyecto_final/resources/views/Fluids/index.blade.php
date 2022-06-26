d65ee8264c73ce0cef0a895118f0f9f4d09fe3c5
MariaJoseGG <mgiraldogu@unal.edu.co>
2022-06-26 10:05:33 -0500

    Se modifica el README

--- proyecto_final/resources/views/Fluids/index.blade.php
+++ proyecto_final/resources/views/Fluids/index.blade.php
@@ -3,29 +3,31 @@
         <form method="POST" action="{{ route('fluids.store') }}">
             @csrf
             <div class="mb-3">
-                <label for="hora" class="form-label">Hora</label>
+                <label for="hora" class="form-label">Cantidad de líquido</label>
                 <input name="input" type="number" class="form-control" id="hora">
             </div>
             <button type="submit" class="btn btn-primary">Ingresar</button>
         </form>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Hora</th>
-                    @foreach($fluids as $fluid)
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Hora</th>
+                        @foreach($fluids as $fluid)
                         <th scope="col">{{ $fluid->hour }}</th>
-                    @endforeach
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">Entrada</th>
-                    @foreach($fluids as $fluid)
+                        @endforeach
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Entrada</th>
+                        @foreach($fluids as $fluid)
                         <td>{{ $fluid->input }}</td>
-                    @endforeach
-                </tr>
-            </tbody>
-        </table>
+                        @endforeach
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         <a href="{{ route('pdf') }}" class="mt-1 btn btn-danger">Generar PDF</a>
     </div>
 </x-app-layouts>
